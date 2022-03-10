3a8b6f539a9a764719db04af8e2f28c6e6e04b60
Zachary Hueras <zhueras@amdtelemedicine.com>
2022-03-09 20:22:49 -0500

    protocol/decoder: eslint --fix

--- protocol/decoder.js
+++ protocol/decoder.js
@@ -3,15 +3,15 @@
  *
  * @see http://www.erlang.org/doc/apps/erts/erl_dist_protocol.html
  */
-'use strict'
+'use strict';
 
-let constants = require('./constants')
+let constants = require('./constants');
 
 class DecoderError extends Error {
-  constructor (message) {
-    super(message)
-    this.name = 'DecoderError'
-  }
+    constructor(message) {
+        super(message);
+        this.name = 'DecoderError';
+    }
 }
 
 /**
@@ -53,16 +53,22 @@ exports.decode = function decode(buf) {
  * @param {Buffer} buf
  * @returns {{code: number, data: {creation: Buffer}}}
  */
-function decodeAliveResponse (buf) {
-  let result = buf.readUInt8(1)
-  if (result > 0) {
-    throw new DecoderError('EPMD returned an error. Result: "' + result + '"')
-  }
+function decodeAliveResponse(buf) {
+    let result = buf.readUInt8(1);
+    if (result > 0) {
+        throw new DecoderError(
+            'EPMD returned an error. Result: "' + result + '"'
+        );
+    }
 
-  return {
-    code: constants.ALIVE_RESP,
-    data: {
-      creation: buf.slice(2)
+    return {
+        code: constants.ALIVE_RESP,
+        data: {
+            creation: buf.slice(2),
+        },
+    };
+}
+
 function decodeAlive2XResponse(buf) {
     let result = buf.readUInt8(1);
     if (result > 0) {
@@ -90,47 +96,53 @@ function decodeAlive2XResponse(buf) {
  * @throws DecoderError
  * @returns {{code: number, data: {port: *, nodeName: (string|*), extra: Buffer}}}
  */
-function decodePortResponse (buf) {
-  let result = buf.readUInt8(1)
-  if (result > 0) {
-    throw new DecoderError('EPMD returned an error. Result: "' + result + '"')
-  }
-
-  let offset = 2
-  let portNo = buf.readUInt16BE(offset)
-  offset += 2
-  let nodeType = buf.readUInt8(offset)
-  offset += 1
-  let protocol = buf.readUInt8(offset)
-  offset += 1
-  let highestVersion = buf.readUInt16BE(offset)
-  offset += 2
-  let lowestVersion = buf.readUInt16BE(offset)
-  offset += 2
-  let nodeLength = buf.readUInt16BE(offset)
-  offset += 2
-  let nodeName = buf.toString('utf8', offset, offset + nodeLength)
-  offset = offset + nodeLength
-  let elen = buf.readUInt16BE(offset)
-  offset = offset + 2
-  let extra = buf.slice(offset, elen)
-
-  if (highestVersion < constants.LOWEST_VERSION) {
-    throw new DecoderError('Highest protocol version returned lower than our lowest version')
-  }
-  if (lowestVersion > constants.HIGHEST_VERSION) {
-    throw new DecoderError('Lowest protocol version provided higher than our highest version')
-  }
-  return {
-    code: constants.PORT2_RESP,
-    data: {
-      nodeType: nodeType,
-      protocol: protocol,
-      port: portNo,
-      name: nodeName,
-      extra: extra
+function decodePortResponse(buf) {
+    let result = buf.readUInt8(1);
+    if (result > 0) {
+        throw new DecoderError(
+            'EPMD returned an error. Result: "' + result + '"'
+        );
     }
-  }
+
+    let offset = 2;
+    let portNo = buf.readUInt16BE(offset);
+    offset += 2;
+    let nodeType = buf.readUInt8(offset);
+    offset += 1;
+    let protocol = buf.readUInt8(offset);
+    offset += 1;
+    let highestVersion = buf.readUInt16BE(offset);
+    offset += 2;
+    let lowestVersion = buf.readUInt16BE(offset);
+    offset += 2;
+    let nodeLength = buf.readUInt16BE(offset);
+    offset += 2;
+    let nodeName = buf.toString('utf8', offset, offset + nodeLength);
+    offset = offset + nodeLength;
+    let elen = buf.readUInt16BE(offset);
+    offset = offset + 2;
+    let extra = buf.slice(offset, elen);
+
+    if (highestVersion < constants.LOWEST_VERSION) {
+        throw new DecoderError(
+            'Highest protocol version returned lower than our lowest version'
+        );
+    }
+    if (lowestVersion > constants.HIGHEST_VERSION) {
+        throw new DecoderError(
+            'Lowest protocol version provided higher than our highest version'
+        );
+    }
+    return {
+        code: constants.PORT2_RESP,
+        data: {
+            nodeType: nodeType,
+            protocol: protocol,
+            port: portNo,
+            name: nodeName,
+            extra: extra,
+        },
+    };
 }
 
 /**
